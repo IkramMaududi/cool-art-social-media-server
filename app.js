544c74ffc15dd20693e8c3e85b68cb4334164b02
IkramMaududi <onime.kyo999@gmail.com>
2021-01-25 15:39:43 +0700

    after register login

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const userRoute = require('./routes/User');
 
 const app = express();
 
+//middleware
+app.use(cors()); //cors is used to allow request from backend to frontend 
 app.use(express.json());
 app.use('/user', userRoute);
 
--- routes/User.js
+++ routes/User.js
@@ -11,12 +11,37 @@ router
 
 router
     .route('/register')
-    .get( async (req,res) => {
+    .post( async (req,res) => {
         try {
-          const newInsert = await pool.query(
-            "INSERT INTO users (username, password) VALUES ('absfdg', 'pword') RETURNING *;"
-          );
-          res.json(newInsert.rows[0]);
+            const {username, password} = req.body;
+            const newInsert = await pool.query(
+                "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *;",
+                [username, password]
+            );
+            res.json(newInsert.rows[0]);
+        } catch (err) {
+            console.error(err.message);
+        };
+    });
+
+router
+    .route('/login')
+    .post( async (req,res) => {
+        try {
+            const {username, password} = req.body;
+            const select = await pool.query(
+                "SELECT * FROM users WHERE username = $1;", 
+                [username]
+            );
+            if (select.rowCount) {
+                if (password == select.rows[0].password) {
+                    res.send("You're logged in");
+                } else {
+                    res.send("Wrong username/password");
+                };
+            } else {
+                res.send("User doesn't exist");
+            };
         } catch (err) {
             console.error(err.message);
         };
